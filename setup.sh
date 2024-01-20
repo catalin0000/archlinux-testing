7c9b0200453afc3cc9fd5501b480585c98d7b1a4
Catalin <62440502+catalin0000@users.noreply.github.com>
2024-01-20 11:42:23 +0000

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+echo "deb http://http.kali.org/kali kali-rolling main non-free contrib" >> /etc/apt/sources.list
+echo "deb http://kali.download/kali kali-rolling main non-free contrib" >> /etc/apt/sources.list
+wget https: //archive.kali.org/archive-key.asc -O /etc/apt/trusted.gpg.d/kali-archive-keyring.asc
+apt update
+apt --fix-broken install
+apt -y upgrade
+apt -y dist-upgrade
+apt -y autoremove --purge
+apt -y install kali-linux-everything
