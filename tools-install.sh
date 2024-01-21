668127dbf4f8ffb63633b4daa05af6acc598859a
Catalin <62440502+catalin0000@users.noreply.github.com>
2024-01-21 16:02:18 +0000

    Update tools-install.sh

--- tools-install.sh
+++ tools-install.sh
@@ -1,4 +1,4 @@
-pacman --noconfirm --disable-download-timeout -S amass wget python-pip nmap sqlmap rustscan feroxbuster burpsuite wireshark-qt nikto hashcat john metasploit tcpdump gnu-netcat hydra waybackurls gau anewer meg qsreplace httprobe assetfinder gf dnsx ffuf gobuster gospider hakrawler httpx metabigor subfinder wafw00f android-apktool secretfinder dalfox nuclei
+pacman -S amass wget python-pip nmap sqlmap rustscan feroxbuster burpsuite wireshark-qt nikto hashcat john metasploit tcpdump gnu-netcat hydra waybackurls gau anewer meg qsreplace httprobe assetfinder gf dnsx ffuf gobuster gospider hakrawler httpx metabigor subfinder wafw00f android-apktool secretfinder dalfox nuclei
 
 mkdir /usr/share/wordlists
 cd /usr/share/wordlists
