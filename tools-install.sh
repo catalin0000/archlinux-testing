0c446139c7233f48eda886d29c9d54efc20983b7
Catalin <62440502+catalin0000@users.noreply.github.com>
2024-01-21 16:01:59 +0000

    Update tools-install.sh

--- tools-install.sh
+++ tools-install.sh
@@ -1,1 +1,6 @@
 pacman --noconfirm --disable-download-timeout -S amass wget python-pip nmap sqlmap rustscan feroxbuster burpsuite wireshark-qt nikto hashcat john metasploit tcpdump gnu-netcat hydra waybackurls gau anewer meg qsreplace httprobe assetfinder gf dnsx ffuf gobuster gospider hakrawler httpx metabigor subfinder wafw00f android-apktool secretfinder dalfox nuclei
+
+mkdir /usr/share/wordlists
+cd /usr/share/wordlists
+git clone https://github.com/danielmiessler/SecLists.git
+cd $MY_DIR
