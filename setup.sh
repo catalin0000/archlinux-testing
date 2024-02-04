7f63f346fda731e7692dff2fbc3ab5dff846db96
Catalin <katanush@gmail.com>
2024-02-04 14:13:46 +0000

    wahtever

--- setup.sh
+++ setup.sh
@@ -3,5 +3,13 @@ pacman-key --lsign-key 3056513887B78AEB
 pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
 pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 
-echo '[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist' >> /etc/pacman.conf
-echo '\n[blackarch]\nSigLevel = Never\nServer = https://blackarch.org/blackarch/$repo/os/$arch\n' >> /etc/pacman.conf
+echo '\n\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist' >> /etc/pacman.conf
+echo '\n\n[blackarch]\nSigLevel = Never\nServer = https://blackarch.org/blackarch/$repo/os/$arch\n' >> /etc/pacman.conf
+
+
+pacman -Syy
+pacman -Syu
+pacman -S zsh
+
+cp .zshrc /home/*/.
+cp .zshrc /root/.
